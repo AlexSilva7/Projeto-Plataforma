8efbef2992ec3457240cf13422fa4888b423cdbd
Alex Silva <alexsaraujo92@gmail.com>
2021-10-13 19:02:43 -0300

    novas funcionalidades

--- DAL/students.js
+++ DAL/students.js
@@ -6,13 +6,39 @@ const studentModel = require('../domain/students')
 const db = require('./dbConnection')
 
 class student {
+    
+    static async create(nome, sobrenome, endereco, data, naturalidade, escolaridade, estadoCivil, parentes, situacaoEmprego, renda, rendaFamiliar, rg, cpf, telefone, email){
 
-    /*
-    static async create(nome, sobrenome, endereco, data, naturalidade, escolaridade, estadoCivil, parentes, situacaoEmprego, renda, rendaFamiliar){
-        await db.query("INSERT INTO students(name, surname, adress, birthdate, place_of_birth, scholarity, marital_status, relatives, employment_status, income, familyinconme) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",[nome],[sobrenome],[endereco],[data],[naturalidade],[escolaridade],[estadoCivil],[parentes],[situacaoEmprego],[renda],[rendaFamiliar])
+        let insertStudent = await db.query("INSERT INTO students(name, surname, adress, birthdate, place_of_birth, scholarity, marital_status, relatives, employment_status, income, familyinconme) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
+        [nome,sobrenome,endereco,data,naturalidade,escolaridade,estadoCivil,parentes,situacaoEmprego,renda,rendaFamiliar])
+
+        var studentId = await db.query("SELECT student_id FROM Students where name = $1 and surname = $2 and adress = $3 and birthdate = $4 and income = $5 and familyinconme = $6 and cast(created_at as varchar(10)) = cast(current_date as varchar(10))", 
+        [nome, sobrenome, endereco, data, renda, rendaFamiliar])
+
+        var id = studentId.rows[0].student_id
+
+        if(telefone !== ''){
+            await db.query("INSERT INTO studentcontacts(student_id, contact_type, contact_description, contact_value) VALUES($1, $2, $3, $4)", 
+            [id, 1, 'Phone', telefone])
+        }
+
+        if(email !== ''){
+            await db.query("INSERT INTO studentcontacts(student_id, contact_type, contact_description, contact_value) VALUES($1, $2, $3, $4)", 
+            [id, 2, 'Email', email])
+        }
+
+        if(rg !== ''){
+            await db.query("INSERT INTO studentdocuments(student_id, document_type, document_description, document_value) VALUES($1, $2, $3, $4)", 
+            [id, 1, 'RG', rg])
+        }
+
+        if(cpf !== ''){
+            await db.query("INSERT INTO studentdocuments(student_id, document_type, document_description, document_value) VALUES($1, $2, $3, $4)", 
+            [id, 2, 'CPF', cpf])
+        }
+
      }
-     */
-
+     
     static async load(studentId) {
         /*
         var students = await db.query("SELECT student_id, name, surname, adress, birthdate, place_of_birth, scholarity, marital_status, relatives, employment_status, income, familyInconme FROM Students where student_id = $1", [studentId])
--- app.js
+++ app.js
@@ -20,15 +20,24 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 var completeName
+var loginADM
+var senhaADM
+var loginSucess
+
 
 app.get('/', async(req, res) => {
     res.render("home/index")
 })
 
 app.post('/admin', async(req, res) => {
+
     let {login, senha} = req.body
-    let pass = await dbconnection.query('SELECT pass FROM admins WHERE login = $1', [login])
-    let nameUser = await dbconnection.query('SELECT name FROM admins WHERE login = $1', [login])
+
+    loginADM = login
+    senhaADM = senha
+
+    let pass = await dbconnection.query('SELECT pass FROM admins WHERE login = $1', [loginADM])
+    let nameUser = await dbconnection.query('SELECT name FROM admins WHERE login = $1', [loginADM])
     let password
 
     completeName
@@ -42,11 +51,21 @@ app.post('/admin', async(req, res) => {
     }
 
     if(senha == password){
-        
         res.render("home/principal", {name:completeName})
 
     }else{
+        //res.redirect('/')
+        res.render("home/indexError")
+    }
+})
+
+app.get('/adm', async(req, res) => {
+    
+    if(loginADM == undefined){
         res.redirect('/')
+    }
+    else{
+        res.render("home/principal", {name:completeName})
     }
 })
 
@@ -54,27 +73,20 @@ app.post('/cadastro', async(req, res) => {
     res.render("home/cadastro", {name:completeName})
 })
 
+app.post('/alunos', async(req, res) => {
+    res.render("home/alunos", {name:completeName})
+})
+
 app.post('/salvarEstudante', async(req, res) => {
 
     let {nome, sobrenome, endereco, data, naturalidade, escolaridade, estadoCivil, parentes, situacaoEmprego,renda, rendaFamiliar, rg, cpf, telefone, email} = req.body
 
-    var result = await dbconnection.query("INSERT INTO students(name, surname, adress, birthdate, place_of_birth, scholarity, marital_status, relatives, employment_status, income, familyinconme) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
-    [nome,sobrenome,endereco,data,naturalidade,escolaridade,estadoCivil,parentes,situacaoEmprego,renda,rendaFamiliar])
+    await studentDal.create(nome,sobrenome,endereco,data,naturalidade,escolaridade,estadoCivil,parentes,situacaoEmprego,renda,rendaFamiliar, rg, cpf, telefone, email)
     
-    res.render("home/principal", {name:completeName})
+    res.redirect('/adm')
 
-    /*
-    let ret = await studentDal.createStudent(nome, sobrenome, endereco, data, naturalidade, escolaridade, estadoCivil, parentes, situacaoEmprego,renda, rendaFamiliar, rg, cpf, telefone, email)
-    res.send(ret)
-    */
 })
 
-/*
-app.get('/create-student', async(req, res) => {
-    let ret = await studentDal.create('nome', 'sobrenome', 'endereco', 'data', 'naturalidade', 'escolaridade', 'estadoCivil', 'parentes', 'situacaoEmprego','renda', 'rendaFamiliar')
-    res.send(ret)
-})
-*/
 
 app.get('/contact', async(req, res) => {
     let ret = await contactDal.load(1)
@@ -86,12 +98,6 @@ app.get('/document', async(req, res) => {
     res.send(ret)
 })
 
-/*
-app.get('/', async(req, res) => {
-    let ret = await studentDal.load(1)
-    res.send(ret)
-})
-*/
 
 app.get('/create-contact', async(req, res) => {
     let ret = await contactDal.create(1, 3, 'instagram', 'alexsilva7')
